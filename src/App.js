9e2e12a537aaa463d5dbffbd8480cf3ddbf41433
Krishna Tandon <krishnatandon.1208@gmail.com>
2020-11-26 01:05:10 +0530

    setState => class based state updation

--- src/App.js
+++ src/App.js
@@ -2,13 +2,44 @@ import React from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-function App() {
-  return (
-    <div className="App">
-      <Person name="Krishna" company="TCS">Hobbies: Coding</Person>
-      <Person name="Kaveri" company="MB-Dubai">Hobbies: Reading</Person>
-    </div>
-  );
+class App extends React.Component {
+
+	state = {
+		persons: [
+			{ name : "Krishna", company: "TCS", hobbies: "Football" },
+			{ name : "Kaveri", company: "MB-Dubai", hobbies: "Reading" }
+		],
+		otherProperty: "Hello"
+	}
+
+	//Creating methods for our class.
+	switchCompanyHandler = () => {
+		this.setState({
+			persons: [
+				{ name : "Krishna", company: "Microsoft", hobbies: "Football" },
+				{ name : "Kaveri", company: "Apple", hobbies: "Reading" }
+			],
+		});
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<Person 
+					name={this.state.persons[0].name} 
+					company={this.state.persons[0].company}>
+					{this.state.persons[0].hobbies}
+				</Person>
+				<Person 
+					name={this.state.persons[1].name} 
+					company={this.state.persons[1].company}>
+					{this.state.persons[1].hobbies}
+				</Person>
+				<h6>{this.state.otherProperty}</h6>
+				<button onClick={this.switchCompanyHandler}>Switch Company</button>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,12 +3,11 @@ import React from "react";
 export default function Person(props) {
     return (
         <div>
-            {/* <h1>People in react learning group</h1> */}
             <div>
                 <p>Name : {props.name}</p>
                 <p>Company: {props.company}</p>
             </div>
-            <p>{props.children}</p>
+            <p>Hobbies: {props.children}</p>
         </div>
     )
 }
