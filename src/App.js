9fe028d91d1da766e3ab2f1965b01f19b3f7cc5a
Krishna Tandon <krishnatandon.1208@gmail.com>
2020-11-27 00:47:48 +0530

    useState implementation part 1

--- src/App.js
+++ src/App.js
@@ -1,45 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-class App extends React.Component {
+function App() {
 
-	state = {
-		persons: [
-			{ name : "Krishna", company: "TCS", hobbies: "Football" },
-			{ name : "Kaveri", company: "MB-Dubai", hobbies: "Reading" }
-		],
-		otherProperty: "Hello"
-	}
+	// state = {
+	// 	persons: [
+	// 		{ name : "Krishna", company: "TCS", hobbies: "Football" },
+	// 		{ name : "Kaveri", company: "MB-Dubai", hobbies: "Reading" }
+	// 	],
+	// 	otherProperty: "Hello"
+	// }
 
 	//Creating methods for our class.
-	switchCompanyHandler = () => {
-		this.setState({
-			persons: [
-				{ name : "Krishna", company: "Microsoft", hobbies: "Football" },
-				{ name : "Kaveri", company: "Apple", hobbies: "Reading" }
-			],
-		});
-	}
+	// switchCompanyHandler = () => {
+	// 	this.setState({
+	// 		persons: [
+	// 			{ name : "Krishna", company: "Microsoft", hobbies: "Football" },
+	// 			{ name : "Kaveri", company: "Apple", hobbies: "Reading" }
+	// 		],
+	// 	});
+	// }
 
-	render() {
-		return (
-			<div className="App">
-				<Person 
-					name={this.state.persons[0].name} 
-					company={this.state.persons[0].company}>
-					{this.state.persons[0].hobbies}
-				</Person>
-				<Person 
-					name={this.state.persons[1].name} 
-					company={this.state.persons[1].company}>
-					{this.state.persons[1].hobbies}
-				</Person>
-				<h6>{this.state.otherProperty}</h6>
-				<button onClick={this.switchCompanyHandler}>Switch Company</button>
-			</div>
-		);
-	}
+	const [personState, setPersonState] = useState({
+		persons: [
+			{ name: "Krishna", company: "TCS", hobbies: "Football" },
+			{ name: "Kaveri", company: "MB-Dubai", hobbies: "Reading" }
+		],
+		otherProperty: "Hellozzzz"
+	});
+
+	return (
+		<div className="App">
+			<Person
+				name={personState.persons[0].name}
+				company={personState.persons[0].company}>
+				{personState.persons[0].hobbies}
+			</Person>
+			<Person
+				name={personState.persons[1].name}
+				company={personState.persons[1].company}>
+				{personState.persons[1].hobbies}
+			</Person>
+			<h6>{personState.otherProperty}</h6>
+			<button>Switch Company</button>
+		</div>
+	);
 }
 
 export default App;
